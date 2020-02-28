2130bd037437d3b5e9379a4ced1c7aad206696f6
unknown <tylerbroere@gmail.com>
2020-02-28 09:46:23 +0100

    fixed recipe index bug

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,17 +1,12 @@
 import { Ingredient } from '../shared/ingredient.model';
 export class Recipe {
-  //  Add static counter to assign ID on constructor??
-  //! ISSUE WITH ID ASSIGNMENT
-  public static countID = 2;
-  public id: number;
   public name: string;
   public description: string;
   public imagePath: string;
   public ingredients: Ingredient[];
 
-  constructor(id: number, name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
 
-    this.id = id ? id : Recipe.countID++;
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,6 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from 'rxjs';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -10,24 +11,26 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      1,
       'Schnitzel',
       'This is a schnitzel',
       'https://tinyurl.com/qw24g4q',
       [new Ingredient('Meat', 1), new Ingredient('Lemon', 2)]
     ),
-    new Recipe(2, 'Salad', 'This is a salad', 'https://tinyurl.com/u8u2m8a', [
+    new Recipe('Salad', 'This is a salad', 'https://tinyurl.com/u8u2m8a', [
       new Ingredient('Tomato', 2),
       new Ingredient('Leafs', 5)
     ])
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
   }
 
-  showSingleRecipe(id: number) {
-    return this.recipes.find(el => el.id === id);
+  showSingleRecipe(index: number) {
+    // return this.recipes.find(el => el.id === id);
+    return this.recipes[index];
   }
 
   addRecipe(recipe: Recipe) {
