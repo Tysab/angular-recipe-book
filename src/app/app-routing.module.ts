26c37c92d9a79917e3a7cd45a395de29ceb58b44
unknown <tylerbroere@gmail.com>
2020-03-09 12:27:24 +0100

    split services into separate modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'shopping-list',
-    component: ShoppingListComponent
-  },
-  {
-    path: 'auth',
-    component: AuthComponent
-  },
-  { path: '**', redirectTo: '/recipes' }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  // { path: '**', redirectTo: '/recipes' }
 ];
 
 @NgModule({
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -39,5 +39,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class RecipesRoutingModule {
-}
+export class RecipesRoutingModule {}
